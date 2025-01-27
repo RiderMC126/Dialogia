0c44d982846b3930fbf957d07cdce0b3acc547af
RiderMC126 <ridermc123@gmail.com>
2025-01-27 16:10:04 +0300

    create db.py

---
+++ db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+conn = sqlite3.connect('db.db')
+cur = conn.cursor()
+
+
+# Создание "users"
+def create_table_users():
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT,
+            password TEXT,
+            date DATE NOT NULL DEFAULT CURRENT_DATE,
+            blocked TEXT NOT NULL DEFAULT NO
+        );
+    ''')
