2c220042b058ffab9565b838ed12010f40f3980f
RiderMC126 <ridermc123@gmail.com>
2025-01-30 16:20:14 +0300

    update

--- main.py
+++ main.py
@@ -279,6 +279,10 @@ def refresh_captcha():
     session['captcha'] = captcha_text
     return redirect(url_for('register'))
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 
 
 if __name__ == '__main__':
