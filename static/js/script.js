8afc6dadc797bfe171fc8cb7ad01f18dd5c85847
TheLast-op <yutubex8@gmail.com>
2025-01-30 11:42:42 +0300

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,10 @@
-
+document.getElementById('avatar-upload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('avatar-image').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
